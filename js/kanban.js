8b95aa592a571a8233ef0eaa8c2abb7465d8c1a2
Demari-Moore <44060033+Demari-Moore@users.noreply.github.com>
2021-10-04 08:19:05 +0100

    Update kanban.js

--- js/kanban.js
+++ js/kanban.js
@@ -18,10 +18,10 @@ add_btn.addEventListener('click', () => {
 const create_item = () => {
     let item = document.createElement('div');
     item.classList.add('item');
-    item.id = 'item' + order;
+    item.id = 'item-' + order;
     item.draggable = true;
     item.addEventListener('dragstart', event => event.dataTransfer.setData('text', event.target.id));
-    item.addEventListener('dragend', event => event.dataTransfer.creatData());
+    item.addEventListener('dragend', event => event.dataTransfer.clearData());
     let input = document.createElement('input');
     item.appendChild(input);
 
